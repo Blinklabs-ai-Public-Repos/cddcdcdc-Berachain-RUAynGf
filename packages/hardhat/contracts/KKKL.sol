7ff791cfce5c5dc62b7d176c1852c9425d9eefef
blinklabs-admin <mike@blinklabs.ai>
2024-07-05 16:54:18 +0800

    Create an ERC-20 token called kkkl

---
+++ packages/hardhat/contracts/KKKL.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract KKKL is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("kkkl", "KKKL") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+}
